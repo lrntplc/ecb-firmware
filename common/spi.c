d9e092e72e6ece7528008b6f27897cbb08a456b9
Laurentiu Palcu <lpalcu@gmail.com>
2016-01-24 18:19:56 +0200

    spi: fix some issues
    Signed-off-by: Laurentiu Palcu <lpalcu@gmail.com>

--- common/spi.c
+++ common/spi.c
@@ -12,27 +12,26 @@ void spi_master_init()
 {
 
 	/* Set MOSI, SCK and SS as Output */
-	DDRB = (1 << PB5) | (1 << PB3) | (1 << PB2);
-	PORTB |= _BV(PB2);
+	DDRB |= (1 << PB5) | (1 << PB3) | (1 << PB2);
 
 	/*
 	 * Enable SPI, Set as Master
-	 * Prescaler: Fosc/2, Enable Interrupts
+	 * Prescaler: Fosc/4, Enable Interrupts
 	 * The MOSI, SCK pins are as per ATMega8
 	 */
-	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPI2X) | ( 1 << SPIE );
+	SPCR = (1 << SPE) | (1 << MSTR) | ( 1 << SPIE );
 }
 
 void spi_master_cs_set()
 {
 	PORTB &= ~_BV(PB2);
 
-	_delay_us(5);
+	_delay_us(1);
 }
 
 void spi_master_cs_clear()
 {
-	_delay_us(5);
+	_delay_us(1);
 
 	PORTB |= _BV(PB2);
 }
